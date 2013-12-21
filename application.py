37d71fdc85acc99a639f546337ac7edcecd07a7a
Dan Sommerfield <sommda@gmail.com>
2013-12-21 15:07:03 -0800

    s/app/application (to keep WSGI happy)

--- application.py
+++ application.py
@@ -4,8 +4,8 @@ from flask import abort
 from flask import make_response
 from flask import request
 
-app = Flask(__name__)
-app.debug=True
+application = Flask(__name__)
+application.debug=True
 
 tasks = [
     {
@@ -22,22 +22,22 @@ tasks = [
     }
 ]
 
-@app.route('/')
+@application.route('/')
 def hello_world():
     return "Hello world!"
 
-@app.route('/todo/api/v1.0/tasks', methods = ['GET'])
+@application.route('/todo/api/v1.0/tasks', methods = ['GET'])
 def get_tasks():
     return jsonify( { 'tasks': tasks } )
 
-@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods = ['GET'])
+@application.route('/todo/api/v1.0/tasks/<int:task_id>', methods = ['GET'])
 def get_task(task_id):
     task = filter(lambda t: t['id'] == task_id, tasks)
     if len(task) == 0:
         abort(404)
     return jsonify( { 'task': task[0] } )
 
-@app.route('/todo/api/v1.0/tasks', methods = ['POST'])
+@application.route('/todo/api/v1.0/tasks', methods = ['POST'])
 def create_task():
     if not request.json or not 'title' in request.json:
         abort(400)
@@ -50,7 +50,7 @@ def create_task():
     tasks.append(task)
     return jsonify( { 'task': task } ), 201
 
-@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods = ['PUT'])
+@application.route('/todo/api/v1.0/tasks/<int:task_id>', methods = ['PUT'])
 def update_task(task_id):
     task = filter(lambda t: t['id'] == task_id, tasks)
     if len(task) == 0:
@@ -68,7 +68,7 @@ def update_task(task_id):
     task[0]['done'] = request.json.get('done', task[0]['done'])
     return jsonify( { 'task': task[0] } )
 
-@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods = ['DELETE'])
+@application.route('/todo/api/v1.0/tasks/<int:task_id>', methods = ['DELETE'])
 def delete_task(task_id):
     task = filter(lambda t: t['id'] == task_id, tasks)
     if len(task) == 0:
@@ -76,9 +76,9 @@ def delete_task(task_id):
     tasks.remove(task[0])
     return jsonify( { 'result': True } )
 
-@app.errorhandler(404)
+@application.errorhandler(404)
 def not_found(error):
     return make_response(jsonify( { 'error': 'Not found' } ), 404)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug = True)
+    application.run(host='0.0.0.0', debug = True)
